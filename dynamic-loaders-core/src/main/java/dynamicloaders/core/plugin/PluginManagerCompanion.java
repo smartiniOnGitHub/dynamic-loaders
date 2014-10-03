09bbde5da3d9355dada93ba9d03d2ae63f06c912
Sandro Martini <sandro.martini@gmail.com>
2014-10-03 18:00:16 +0200

    add license header to all sources, and updated docs

--- dynamic-loaders-core/src/main/java/dynamicloaders/core/plugin/PluginManagerCompanion.java
+++ dynamic-loaders-core/src/main/java/dynamicloaders/core/plugin/PluginManagerCompanion.java
@@ -1,3 +1,19 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except in
+ * compliance with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package dynamicloaders.core.plugin;
 
 import java.util.ArrayList;
@@ -184,7 +200,7 @@ public class PluginManagerCompanion {
 	 * 
 	 * @param name the full class name of the plugin
 	 * @param loader the class loader to be used, or <tt>null</tt> to use a default one
-	 * @return
+	 * @return a new Plugin instance, or null
 	 */
 	public static Plugin createPluginFromClass(final String name, final ClassLoader loader) {
 		Objects.requireNonNull(name);
